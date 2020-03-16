c9d406a6721c62db7eb8016d8acd1198faa1b8af
Patrick O'Brien <patricknathanobrien@gmail.com>
2020-03-16 19:46:40 -0400

    Final Commit

---
+++ assets/javascript/giphy-generator.js
@@ -0,0 +1,76 @@
+var topics = ["Legend of Zelda", "Super Mario", "Sonic the Hedgehog", "Fallout", "Final Fantasy",
+"World of Warcraft", "Street Fighter", "Starcraft", "Mega Man", "Bioshock",
+"Castlevania", "Red Dead Redemption", "Untitled Goose Game", "The Elder Scrolls", "God of War",
+"Uncharted", "Pokemon", "Kingdom Hearts"];
+var numberOfGIFs = 10;
+var cutOffRating = "PG";
+
+function renderButtons(){
+	for(var i = 0; i < topics.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("game-button");
+		newButton.text(topics[i]);
+		$("#button-container").append(newButton);
+	}
+	$(".game-button").unbind("click");
+
+	$(".game-button").on("click", function(){
+		$(".gif-image").unbind("click");
+		$("#gif-container").empty();
+		$("#gif-container").removeClass("dotted-border");
+		populateGIFContainer($(this).text());
+	});
+
+}
+
+function addButton(show){
+	if(topics.indexOf(show) === -1) {
+		topics.push(show);
+		$("#button-container").empty();
+		renderButtons();
+	}
+}
+
+function populateGIFContainer(show){
+	$.ajax({
+		url: "https://api.giphy.com/v1/gifs/search?q=" + show + 
+		"&api_key=61TKqzUDPHfv40Bqr6iEsqqBCfa360mt&rating=" + cutOffRating + "&limit=" + numberOfGIFs,
+		method: "GET"
+	}).then(function(response){
+		response.data.forEach(function(element){
+			newDiv = $("<div>");
+			newDiv.addClass("individual-gif-container");
+			newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+			var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
+			newImage.addClass("gif-image");
+			newImage.attr("state", "still");
+			newImage.attr("still-data", element.images.fixed_height_still.url);
+			newImage.attr("animated-data", element.images.fixed_height.url);
+			newDiv.append(newImage);
+			$("#gif-container").append(newDiv);
+		});
+		
+		$("#gif-container").addClass("dotted-border");
+		$(".gif-image").unbind("click");
+		$(".gif-image").on("click", function(){
+			if($(this).attr("state") === "still") {
+				$(this).attr("state", "animated");
+				$(this).attr("src", $(this).attr("animated-data"));
+			}
+			else {
+				$(this).attr("state", "still");
+				$(this).attr("src", $(this).attr("still-data"));
+			}
+		});
+	});
+}
+
+$(document).ready(function(){
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		addButton($("#video-game").val().trim());
+		$("#video-game").val("");
+	});
+});
